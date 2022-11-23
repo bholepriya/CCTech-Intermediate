5ea216e3a6bbc55f1ef5dce9caa474171eed7208
bholepriya <106966112+bholepriya@users.noreply.github.com>
2022-11-23 13:34:33 +0530

    Add files via upload

---
+++ CCTech-Intermediate/CalculatorDoubleInterger.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <cassert>
+#include <Climits>
+#include<iomanip>
+
+using namespace std;
+
+void checkValidation(double num1, double num2)
+{
+    if ((num1 > __DBL_MAX__ || num1 < __DBL_MIN__) || (num2 > __DBL_MAX__ || num2 < __DBL_MIN__))
+    {
+        throw runtime_error("Math error: Input value out of range ");
+    }
+}
+
+void addition(double number1, double number2)
+{
+    if ((number1 >= __DBL_MAX__ && number2 > 0) || (number2 >= __DBL_MAX__ && number1 > 0))
+    {
+       double addition = 0;
+        if ((addition = number1 + number2) >= __DBL_MAX__)
+        {
+            throw runtime_error("math error :addition out of range");
+        }
+        else
+        {
+            addition = number1 + number2;
+            cout  << "\nsum : " << addition;
+        }
+    }
+    else
+    {
+        cout << "\nAddition: " << number1 + number2;
+    }
+}
+
+void substraction(double number1, double number2)
+{
+    double diff;
+    if ((number2 > 0 && number1 < __DBL_MIN__ + number2) || (number2 < 0 && number1 < __DBL_MIN__ + number2))
+    {
+        throw runtime_error("math error: Input value out of range ");
+    }
+    else
+    {
+        diff = number1 - number2;
+    }
+    cout << endl
+         << "substraction : " << diff;
+}
+
+void multiplication(double number1, double number2)
+{
+    double mult = number1 * number2;
+    if ((mult > __DBL_MAX__) || (mult < __DBL_MIN__))
+    {
+        throw runtime_error("math error: Input value out of range");
+    }
+    else
+    {
+        cout << "\nMultiplication : " << mult;
+    }
+}
+
+void division(double number1, double number2)
+{
+    if (number2 == 0 || ((number1 == __DBL_MIN__) && (number2 == -1)))
+    {
+        throw runtime_error("math error: Denominator may not be zero");
+    }
+    else
+    {
+        cout << "\nDivision is :- " << number1 / number2 << endl;
+        ;
+    }
+}
+
+int main()
+{
+    char choice;
+
+    try
+    {
+        do
+        {
+            double num1, num2;
+            cout << endl
+                 << "Print A for addition " << endl;
+            cout << "Print S for substraction" << endl;
+            cout << "Print M for multiplication" << endl;
+            cout << "Print D for division " << endl;
+            cout << "Print E for exit" << endl;
+
+            cin >> choice;
+
+            switch (choice)
+            {
+
+            case 'E':
+            {
+                cout << endl
+                     << "ThankYou!!" << endl;
+                exit(0);
+                break;
+            }
+
+            case 'A':
+            {
+                cout << "Enter the two numbers :- " << endl;
+                cin >> num1;
+                cin >> num2;
+                checkValidation(num1, num2);
+                addition(num1, num2);
+                break;
+            }
+            case 'S':
+            {
+                cout << "Enter the two numbers :- " << endl;
+                cin >> num1 >> num2;
+                //checkValidation(num1, num2);
+                substraction(num1, num2);
+                break;
+            }
+            case 'M':
+            {
+                cout << "Enter the two numbers :- " << endl;
+                cin >> num1 >> num2;
+                checkValidation(num1, num2);
+                multiplication(num1, num2);
+                break;
+            }
+            case 'D':
+            {
+                cout << "Enter the two numbers :- " << endl;
+                cin >> num1 >> num2;
+                checkValidation(num1, num2);
+                division(num1, num2);
+                break;
+            }
+            }
+        } while (choice != 0);
+    }
+
+    catch (runtime_error &e)
+    {
+        cout << "Exception occurred" << endl
+             << e.what();
+    }
+    return 0;
+}
